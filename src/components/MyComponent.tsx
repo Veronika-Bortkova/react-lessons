8e81b83e5b6e4564fb8b74953ee87138d73565a2
Veronika-Bortkova <shishiga79@gmail.com>
2026-04-27 15:38:46 +0300

    lesson1 Tailwind 1

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -1,7 +1,7 @@
 import type {FC} from "react";
 // import "./MyComponent.css" это стандартное подключение файла стилей. Если нужно подключить модульные стили, то имя файла в котором они лежат должно быть типа MyComponent.module.css и подключается оно по другому
 
-import styles from "./MyComponent.module.css"
+
 type MyComponentType = {
     text:string;
 }
@@ -12,7 +12,7 @@ const MyComponent:FC<MyComponentType> = ({text}) => {
         //     {text}
         // </div> // так вызываем когда стили обычным файлом стилей заданы
 
-        <div className={styles.target}>
+        <div className={"text-3xl font-bold underline"}>
             {text}
         </div>
     );
