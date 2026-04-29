5d9a2a534d08030085e0b25cdb3199597118087c
Veronika-Bortkova <shishiga79@gmail.com>
2026-04-29 11:30:35 +0300

    dz 1-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import {coursesTitleArray} from "./data/coursesTitleArray.ts";
+
 
 function App() {
 
   return (
     <>
+        <ul>
+            {
+                coursesTitleArray.map((course, index) => <li key={index}>{course}</li>)
+
+
+            }
+
+        </ul>
 
     </>
   )
