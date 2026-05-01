c58c0904f024377fe7da465dd138ab05105b7986
Veronika-Bortkova <shishiga79@gmail.com>
2026-05-01 14:43:02 +0300

    lesson2/env

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,7 +1,19 @@
 import type {IUser} from "../models/userInterface.ts";
-
+// const BaseUrl = import.meta.env.VITE_API_URL;
 export const getUsers = async ():Promise<IUser[]> => {
 
-    return await fetch("https://jsonplaceholder.typicode.com/users")
-        .then(value => value.json());;
+    return await fetch(import.meta.env.VITE_API_URL)
+        .then(value => value.json());
 }
+
+
+export const getUser = async (id:string):Promise<IUser> =>{
+    return await fetch(import.meta.env.VITE_API_URL+"/"+id)
+        .then(value => value.json());
+}
+
+//можно єкспоорт обьявлять так
+// export {
+//getUsers,
+//getUser
+//}
