a92da5171400fd4c30e92bf6a1a8cc56cc885d52
Veronika-Bortkova <shishiga79@gmail.com>
2026-06-04 13:13:18 +0300

    M 4

--- src/constants/urls.ts
+++
@@ -1,7 +0,0 @@
-// const baseUrl = "https://dummyjson.com";
-//
-// export const UrlDummy = {
-//     arrUsers: baseUrl + "/users",
-//     arrCarts: baseUrl + "/carts",
-//     cart: baseUrl + "/carts/user"
-// }
