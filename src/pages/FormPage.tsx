0017b0f6b724029b5766fde4926e6ece8b5e73be
Veronika-Bortkova <shishiga79@gmail.com>
2026-06-05 19:21:32 +0300

    Lesson 5 useForm hook

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,45 +1,25 @@
-import {type FormEvent, useState} from "react";
+import {useForm} from "react-hook-form";
+
 
 interface IFormParam {
     username: string;
-    password: string
+    password: string;
+    age: number
 }
 
 const FormPage = () => {
-    const [formState, setFormState] = useState<IFormParam>({username:"default value", password:"1111"});
-    // const handleUsernameChange = (e:FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, username:input.value});
-    //     console.log(input.value);
-    //
-    // }
-    //
-    // const handlePasswordChange = (e:FormEvent<HTMLInputElement>) => {
-    //     const input = e.target as HTMLInputElement;
-    //     setFormState({...formState, password:input.value});
-    //     console.log(input.value);
-    //
-    // }
-    const handlSubmit = (e: FormEvent<HTMLFormElement>) =>{
-        e.preventDefault();
-        let user = {
-            username: formState.username,
-            password: formState.password
-        }
-        console.log(user);
-    }
-    const handleInputChange = (e:FormEvent<HTMLInputElement>) => {
-        const input = e.target as HTMLInputElement;
-        console.log(input.name);
-        setFormState({...formState, [input.name]:input.value});//в реакте [] такой формой записи можно записать динамическое поле - тоесть в каком инпуте будет отрабатывать функция хндлер сейчас то имя и будет подставляться - то есть измеения там и будут
+    const {handleSubmit, register} = useForm<IFormParam>();
+    const customHandler = (formDataProps:IFormParam)=>{
+        console.log(formDataProps);
+    };
 
-    }
 
     return (
         <div>
-        <form onSubmit={handlSubmit}>
-            <input type="text" name="username" value={formState.username} onChange={handleInputChange}/>
-            <input type="text" name="password" value={formState.password} onChange={handleInputChange}/>
+        <form onSubmit={handleSubmit(customHandler)}>
+            <input type="text" {...register("username")} />
+            <input type="text" {...register("password")}/>
+            <input type="number" {...register("age")}/>
             <button>Send</button>
         </form>
         </div>
