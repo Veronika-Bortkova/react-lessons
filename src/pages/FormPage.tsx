635e0b71654f611ea34d93fb36b0dededec886ae
Veronika-Bortkova <shishiga79@gmail.com>
2026-06-06 23:59:12 +0300

    Lesson 5 useForm Joi

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,4 +1,6 @@
 import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import userValidator from "../validators/userValidator.ts";
 interface IFormParam {
     username: string;
     password: string;
@@ -10,7 +12,7 @@ const FormPage = () => {
         handleSubmit,
         register,
         formState: {errors, isValid},
-    } = useForm<IFormParam>({mode: "all"});//внутри функции юзформ передаем обьект настроек - в нем можно выбрать на какие действия будет проходить валидация формы
+    } = useForm<IFormParam>({mode: "all", resolver: joiResolver(userValidator)});//внутри функции юзформ передаем обьект настроек - в нем можно выбрать на какие действия будет проходить. joiResolver — это специальный адаптер, который берет правила из userValidator и переводит их на «язык», понятный для react-hook-form валидация формы
     const customHandler = (formDataProps:IFormParam)=>{
         console.log(formDataProps);
     };
@@ -19,36 +21,29 @@ const FormPage = () => {
     return (
         <div>
         <form onSubmit={handleSubmit(customHandler)}>
+            <div>
             <label>
-            <input type="text" {...register("username", {
-                required: true, //говорит о том что єто обзательное поле
-                pattern: {
-                    value: /\w+/,//в паттерн можем заполнить как обьект - тогда кроме велью можем сразу прописать, что выдавать при ошибке валидации. опшн - єто обьект, віставляется как второй аргумент функции реджистер
-                    message: "wrong name",
-                },
-                maxLength: {
-                    value: 7,
-                    message: "wrong name"
-                }
-            })} />
+            <input type="text" {...register("username")}/>
                 {errors.username && <div>{errors.username.message}</div>}
             </label>
-            <label><input type="text" {...register("password", {
-                required: true,
-                minLength:{value:3, message: "pass too short"},// минимальная длина слова
-                maxLength: {value:6, message: "parr too long"},// максимальная длина слова
-            })}/>
+            </div>
+
+
+            <div>
+            <label>
+                <input type="text" {...register("password")}/>
                 {errors.password && <div>{errors.password.message}</div>}
             </label>
+            </div>
+
+
+            <div>
             <label>
-            <input type="number" {...register("age", {
-                required: true,
-                valueAsNumber: true,
-                min: {value: 18, message: "age too small"},//проверка именно размера числа - НЕ длина слова
-                max: {value: 117, message: "age too big"}
-            })}/>
+            <input type="number" {...register("age")}/>
                 {errors.age && <div>{errors.age.message}</div>}
             </label>
+            </div>
+
             <button disabled={!isValid}>Send</button>
         </form>
         </div>
